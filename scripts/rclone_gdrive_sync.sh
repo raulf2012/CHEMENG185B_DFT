f27476c11d7fa3c1aa96572a739be9091f86fa67
Raul Flores <raulf2012@gmail.com>
2020-01-22 20:01:06 -0800

    init commit of rclone sync script for GDrive team drive

---
+++ scripts/rclone_gdrive_sync.sh
@@ -0,0 +1,7 @@
+# /home/flores12/GoogleDrive/TeamDrives/chemeng_185b/Computational_DFT
+# /home/flores12/GoogleDrive/TeamDrives/chemeng_185b/Computational_DFT
+
+rclone copy \
+gdrive_chemeng185b:Computational_DFT \
+/home/flores12/GoogleDrive/TeamDrives/chemeng_185b/Computational_DFT \
+--verbose
